bb81668ccd173a47abfc4d1ddd0f122456aca5c6
Bharat Kumawat <bharatkumawatk1@gmail.com>
2024-08-12 14:26:46 +0530

    sidebar updated

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,73 +3,137 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { CATEGORIES_API } from "../utils/constants";
 
+import {
+  HomeIcon,
+  ShortsIcon,
+  SubscriptionsIcon,
+  TrendingIcon,
+  ShoppingIcon,
+  MusicIcon,
+  MoviesIcon,
+  LiveIcon,
+  GamingIcon,
+  NewsIcon,
+} from '../utils/icons';
+
 const Sidebar = () => {
   const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
   const [categories, setCategories] = useState([]);
   useEffect(() => {
     getCategories();
-  },[]); 
+  }, []);
   const getCategories = async () => {
     const data = await fetch(CATEGORIES_API);
     const json = await data.json();
     // console.log(json);
     // setVideos(json.items);
-
-};
+  };
 
   if (!isMenuOpen) return null;
   return (
     <div className="w-1/6 md: fixed h-screen  left-0 dark:text-white bg-white dark:bg-neutral-950 pt-4 hover:overflow-y-auto">
-      <div className="border-b-2 p-4">
-        <h1>
-          <Link to="/"> Home</Link>
-        </h1>
-        <h1><Link to="/"> Shorts</Link></h1>
-        <h1>Subscriptions</h1>
+      <div className=" px-4">
+        <ul>
+          <li className="mb-1">
+            <Link
+              to={"/"}
+              className="flex items-center p-2 dark:text-white  rounded-lg hover:bg-gray-200 dark:hover:bg-neutral-800"
+            >
+              <HomeIcon className="w-5 h-5 mr-3 fill-black dark:fill-white" />
+              <span>Home</span>
+            </Link>
+          </li>
+          <li className="mb-1">
+            <a
+              href="#"
+              className="flex items-center p-2 dark:text-white rounded-lg hover:bg-gray-200  dark:hover:bg-neutral-800"
+            >
+              <ShortsIcon className="w-5 h-5 mr-3 fill-black dark:fill-white" />
+              <span>Shorts</span>
+            </a>
+          </li>
+          <li className="mb-1">
+            <a
+              href="#"
+              className="flex items-center p-2 dark:text-white rounded-lg hover:bg-gray-200  dark:hover:bg-neutral-800"
+            >
+              <SubscriptionsIcon className="w-5 h-5 mr-3 fill-black dark:fill-white" />
+              <span>Subscriptions</span>
+            </a>
+          </li>
+        </ul>
       </div>
-      <div className="p-4">
-        <h1 className="font-semibold">Explore</h1>
-        <ul className="">
-          <li>Trending</li>
-          <li>Shopping</li>
-          <li>Music</li>
-          <li>Films</li>
-          <li>Live</li>
-          <li>Gaming</li>
-          <li>News</li>
-          <li>Sports</li>
-          <li>Learning</li>
-          <li>Fashion & beauty</li>
-          <li>Trending</li>
-          <li>Shopping</li>
-          <li>Music</li>
-          <li>Films</li>
-          <li>Live</li>
-          <li>Gaming</li>
-          <li>News</li>
-          <li>Sports</li>
-          <li>Learning</li>
-          <li>Fashion & beauty</li>
-          <li>Trending</li>
-          <li>Shopping</li>
-          <li>Music</li>
-          <li>Films</li>
-          <li>Live</li>
-          <li>Gaming</li>
-          <li>News</li>
-          <li>Sports</li>
-          <li>Learning</li>
-          <li>Fashion & beauty</li>
-          <li>Trending</li>
-          <li>Shopping</li>
-          <li>Music</li>
-          <li>Films</li>
-          <li>Live</li>
-          <li>Gaming</li>
-          <li>News</li>
-          <li>Sports</li>
-          <li>Learning</li>
-          <li>Fashion & beauty</li>
+      <div className="border-t border-gray-700 my-4"></div>
+      <div className="px-4">
+        {/* <div className="border-t border-gray-700 my-4"></div> */}
+
+        <h2 className="px-2 mb-2 text-lg  font-semibold text-gray-800 dark:text-gray-400">
+          Explore
+        </h2>
+        <ul>
+          <li className="mb-1">
+            <a
+              href="#"
+              className="flex items-center p-2 dark:text-white rounded-lg hover:bg-gray-200  dark:hover:bg-neutral-800"
+            >
+              <TrendingIcon className="w-5 h-5 mr-3 fill-black dark:fill-white" />
+              <span>Trending</span>
+            </a>
+          </li>
+          <li className="mb-1">
+            <a
+              href="#"
+              className="flex items-center p-2 dark:text-white rounded-lg hover:bg-gray-200  dark:hover:bg-neutral-800"
+            >
+              <ShoppingIcon className="w-5 h-5 mr-3 fill-black dark:fill-white" />
+              <span>Shopping</span>
+            </a>
+          </li>
+          <li className="mb-1">
+            <a
+              href="#"
+              className="flex items-center p-2 dark:text-white rounded-lg hover:bg-gray-200  dark:hover:bg-neutral-800"
+            >
+              <MusicIcon className="w-5 h-5 mr-3 fill-black dark:fill-white" />
+              <span>Music</span>
+            </a>
+          </li>
+          <li className="mb-1">
+            <a
+              href="#"
+              className="flex items-center p-2 dark:text-white rounded-lg hover:bg-gray-200  dark:hover:bg-neutral-800"
+            >
+              <MoviesIcon className="w-5 h-5 mr-3 fill-black dark:fill-white" />
+              <span>Movies</span>
+            </a>
+          </li>
+          <li className="mb-1">
+            <a
+              href="#"
+              className="flex items-center p-2 dark:text-white rounded-lg hover:bg-gray-200  dark:hover:bg-neutral-800"
+            >
+              <LiveIcon className="w-5 h-5 mr-3 fill-black dark:fill-white" />
+              <span>Live</span>
+            </a>
+          </li>
+          <li className="mb-1">
+            <a
+              href="#"
+              className="flex items-center p-2 dark:text-white rounded-lg hover:bg-gray-200  dark:hover:bg-neutral-800"
+            >
+              <GamingIcon className="w-5 h-5 mr-3 fill-black dark:fill-white" />
+              <span>Gaming</span>
+            </a>
+          </li>
+          <li className="mb-1">
+            <a
+              href="#"
+              className="flex items-center p-2 dark:text-white rounded-lg hover:bg-gray-200  dark:hover:bg-neutral-800"
+            >
+              <NewsIcon className="w-5 h-5 mr-3 fill-black dark:fill-white" />
+              <span>News</span>
+            </a>
+          </li>
         </ul>
       </div>
     </div>
--- src/utils/searchSlice.js
+++ src/utils/searchSlice.js
@@ -3,15 +3,25 @@ import { createSlice } from "@reduxjs/toolkit";
 const searchSlice = createSlice({
     name: "search",
     initialState:{
-
+        searchQuery: "",
     },
     reducers: {
         cacheResults: (state, action) => {
             // state = {...action.payload, ...state};
             state = Object.assign(state, action.payload);
         },
+        getSearchQuery: (state, action) => {
+            state.searchQuery = action.payload;
+        },
+        clearSearchQuery: (state, action) => {
+            state.searchQuery = "";
+        },
     },
 });
 
-export const {cacheResults} = searchSlice.actions;
+export const {
+    cacheResults,
+    getSearchQuery,
+    clearSearchQuery,
+} = searchSlice.actions;
 export default searchSlice.reducer;
